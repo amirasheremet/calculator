1329d99daadedd1edce7816e39e0ef322966298d
amirasheremet <167184688+amirasheremet@users.noreply.github.com>
2024-09-24 11:52:53 +0200

    Create comments.go

---
+++ comments.go
@@ -0,0 +1,64 @@
+// the sum of two numbers
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	fmt.Print("Enter first number: ")
+	fmt.Scanln(&num1)
+	fmt.Print("Enter second number: ")
+	fmt.Scanln(&num2)
+	sum := num1 + num2
+	fmt.Printf("The sum of %d and %d is %d\n", num1, num2, sum)
+}
+// This is a comment
+// This is another comment
+// This is a third comment
+// This is a fourth comment
+// This is a fifth comment
+// This is a sixth comment
+// This is a seventh comment
+// This is an eighth comment
+// This is a ninth comment
+// This is a tenth comment
+// This is an eleventh comment
+// This is a twelfth comment
+// This is a thirteenth comment
+// This is a fourteenth comment
+// This is a fifteenth comment
+// This is a sixteenth comment
+// This is a seventeenth comment
+// This is an eighteenth comment
+// This is a nineteenth comment
+// This is a twentieth comment
+// This is a twenty-first comment
+// This is a twenty-second comment
+// This is a twenty-third comment
+// This is a twenty-fourth comment
+// This is a twenty-fifth comment
+// This is a twenty-sixth comment
+// This is a twenty-seventh comment
+// This is a twenty-eighth comment
+// This is a twenty-ninth comment
+// This is a thirtieth comment
+// This is a thirty-first comment
+// This is a thirty-second comment
+// This is a thirty-third comment
+// This is a thirty-fourth comment
+// This is a thirty-fifth comment
+// This is a thirty-sixth comment
+// This is a thirty-seventh comment
+// This is a thirty-eighth comment
+// This is a thirty-ninth comment
+// This is a fortieth comment
+// This is a forty-first comment
+// This is a forty-second comment
+// This is a forty-third comment
+// This is a forty-fourth comment
+// This is a forty-fifth comment
+// This is a forty-sixth comment
+// This is a forty-seventh comment
+// This is a forty-eighth comment
+// This is a forty-ninth comment
+// This is a fiftieth
