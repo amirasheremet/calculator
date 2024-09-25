fc74258dc06c0d829fa98e031448ecf520d8000b
amirasheremet <167184688+amirasheremet@users.noreply.github.com>
2024-09-25 16:11:24 +0200

    Create more120.go

---
+++ more120.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, world!")
+}
+
+func longerThan120Characters() {
+  fmt.Println("This function is longer than 120 characters, as it contains a long string that exceeds the character limit.")
+}
